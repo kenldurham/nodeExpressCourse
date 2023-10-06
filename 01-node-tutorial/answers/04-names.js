b31730622343669836e0f2277e144ebfaa02a8d7
Kenneth <kenldurham@gmail.com>
2023-10-06 08:43:05 -0700

    My push with actual work done

---
+++ 01-node-tutorial/answers/04-names.js
@@ -0,0 +1,9 @@
+//local
+const secret = 'SUPER SECRET'
+
+//share
+const ann = 'ann'
+const jeff = 'jeff'
+
+
+module.exports = {ann, jeff}
---
+++ 01-node-tutorial/answers/06-alt-sabor.js
@@ -0,0 +1,7 @@
+const items = ['item1', 'item2']; 
+
+const person = {
+    name: 'bob',
+}
+
+module.exports.singlePeron = person
